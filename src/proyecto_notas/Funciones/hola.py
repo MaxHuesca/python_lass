6f5fb6cbf2e79322d348fb739fed90f004735ad6
MaxHuesca <ismadlsh@lcg.unam.mx>
2025-04-25 12:32:05 -0600

    definiendo funciones gg's ez con parametros y no en hola.py

---
+++ src/proyecto_notas/Funciones/hola.py
@@ -0,0 +1,10 @@
+#Funcion sin parametro por ahi le dicen procedimientos
+def di_hola(): 
+    print("hola")
+    
+di_hola() 
+#Funcion con un parametro 
+def saludar_nombre(nombre):
+    print(f"Hola {nombre}")
+
+saludar_nombre("Valita hermosa")
